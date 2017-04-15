a8a99dc7755e2c680a1381cea6ce5991f5005691
Covina <jabs303@gmail.com>
2017-04-15 14:42:41 -0700

    Section 3, Lecture 22;  Created moving platform.

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour {
+
+	[SerializeField] private float platformMovementSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// move the ground to the left
+		transform.Translate(Vector3.right * (platformMovementSpeed * Time.deltaTime)); 
+
+
+
+
+	}
+}
