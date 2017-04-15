c1a89822c70a92401e16c2fa7739868ca6f35484
Covina <jabs303@gmail.com>
2017-04-15 15:39:34 -0700

    Section 3, Lecture 23;  Lava rock going up and down and with rotation.

---
+++ Assets/Scripts/LavaRock.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaRock : MonoBehaviour {
+
+	[SerializeField] private Vector3 topPos;
+	[SerializeField] private Vector3 bottomPos;
+	[SerializeField] private float rockMovementSpeed = 2f;
+	private float rockRotationSpeed = 50f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine( Move(bottomPos) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// rotate the lava rock
+		transform.Rotate(0, Time.deltaTime * rockRotationSpeed, 0);
+
+												
+	}
+
+
+	IEnumerator Move (Vector3 target)
+	{
+
+
+		// check diff on the Y
+		while(Mathf.Abs( (target - transform.localPosition).y) > 0.20f) {
+
+			// ternary operator
+			Vector3 direction = (target.y == topPos.y) ? Vector3.up : Vector3.down;
+
+			// move the object
+			transform.localPosition += direction * Time.deltaTime * rockMovementSpeed;
+
+
+
+			yield return null;
+
+		}
+
+		Debug.Log("Reached Function");
+
+		yield return new WaitForSeconds (0.5f);
+
+		Vector3 newTarget = (target.y == topPos.y) ? bottomPos : topPos;
+
+		StartCoroutine( Move(newTarget) );
+
+	}
+
+}
--- Assets/Scripts/PlatformMover.cs
+++ Assets/Scripts/PlatformMover.cs
@@ -6,18 +6,35 @@ public class PlatformMover : MonoBehaviour {
 
 	[SerializeField] private float platformMovementSpeed = 3f;
 
+	// Where on the left will it trigger
+	private float resetTriggerPositionX = -22.0f;
+
+	// Where on the right does it move to.
+	private float resetLocationPositionX = 27.0f;
+
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 		// move the ground to the left
-		transform.Translate(Vector3.right * (platformMovementSpeed * Time.deltaTime)); 
+		transform.Translate (Vector3.right * (platformMovementSpeed * Time.deltaTime)); 
 
+		// if its far to the left, move it to the end on the right.
+		if (transform.position.x <= resetTriggerPositionX) {
 
+			// create new start location
+			Vector3 newPos = new Vector3(resetLocationPositionX, transform.position.y, transform.position.z);
+
+			// move it.
+			transform.position = newPos;
+
+		}
 
 
 	}
