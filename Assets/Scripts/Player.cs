ed472dcd7fecddfa54bd2089f6be7ba159a44934
Covina <jabs303@gmail.com>
2017-04-15 22:12:16 -0700

    Section 3, Lecture 28;  Added some sounds, obstacle collisions;

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	private Animator animator;
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+	private float jumpForce = 75f;
+	private bool isJumping = false;
+
+	[SerializeField] AudioClip sfxJump;
+	[SerializeField] AudioClip sfxDeath;
 
 
 	// Use this for initialization
 	void Start () {
 
 		animator = GetComponent<Animator>();
-
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -21,8 +30,52 @@ public class Player : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0) ) {
 			//Debug.Log("Play Jump ANimation");
 			animator.Play("Jump");
+			rigidBody.useGravity = true;
+			isJumping = true;
+
 
 		}
 
 	}
+
+
+	void FixedUpdate ()
+	{
+		if (isJumping) {
+
+			isJumping = false;
+			rigidBody.velocity = new Vector2(0,0);
+
+			rigidBody.AddForce(new Vector2 (0, jumpForce), ForceMode.Impulse );
+
+			// play jump sound
+			audioSource.PlayOneShot(sfxJump);
+
+		}
+
+
+	}
+
+	// PLayer dies when it collides with the obstacle.
+	void OnCollisionEnter (Collision collision)
+	{
+		Debug.Log("Player collided with " + collision.gameObject.name);
+		
+		if (collision.gameObject.tag == "Obstacle") {
+
+			// bounce player
+			rigidBody.AddForce (new Vector2(-50, 20), ForceMode.Impulse);
+
+			// kill Player
+			//Destroy(gameObject);
+			audioSource.PlayOneShot(sfxDeath);
+
+			//Time.timeScale = 0f;
+		}
+
+
+	}
+
+
+
 }
