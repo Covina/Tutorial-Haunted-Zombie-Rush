c51f24692fea3ec3a8efc7492dfc259f79777021
Covina <jabs303@gmail.com>
2017-04-16 22:03:37 -0700

    Added smoe subtle background animations to make some rocks feel alive

---
+++ Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour {
+
+	private int direction = 1;
+	private float startingY;
+	private float movementDistance = 0.25f;
+
+	private float movementSpeedDivisor = 10;
+
+	// Use this for initialization
+	void Start () {
+
+		// get the starting y position
+		startingY = transform.position.y;
+
+		// randomize the movement speed;  higher is slower
+		movementSpeedDivisor = Random.Range(8f, 16f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		// get y
+		float currentPosY = transform.position.y;
+		Vector3 movement;
+
+		if (currentPosY >= (startingY + movementDistance)) direction = -1;
+		if (currentPosY <= (startingY - movementDistance)) direction = 1;
+
+		transform.Translate(new Vector3(0,direction * (Time.deltaTime/movementSpeedDivisor),0) );
+
+	}
+}
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
+
+	private Text bestScoreValue;
 
 	// Called when clicking PLAY from Main Menu
 	public void StartGame ()
@@ -13,5 +16,18 @@ public class MainMenuController : MonoBehaviour {
 
 		SceneManager.LoadScene("Gameplay");
 	}
+
+
+	void Start ()
+	{
+		bestScoreValue = GameObject.Find("BestScoreValue").GetComponent<Text>();
+
+		int tmp = GameManager.instance.GetBestScore();
+
+		bestScoreValue.text = (tmp > 0) ? tmp.ToString() : "TBD";
+		
+
+	}
+
 
 }
